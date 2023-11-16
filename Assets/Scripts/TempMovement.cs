ffa4491720b159ab85ab88a3592c022cd19f0736
Wendy <wchen188@ucsc.edu>
2023-11-15 22:08:13 -0800

    added player movement

--- Assets/Scripts/TempMovement.cs
+++ Assets/Scripts/TempMovement.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class TempMovement : MonoBehaviour
 {
-    public float speed = 15f;
-    public Rigidbody rb;
+    public float forwardSpeed = 15f;
+    public float jumpForce = 4f;
+    public int playerSpeed = 6;
+    private bool isTouchGround = true;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+    }
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove);
+        rb.velocity = new Vector3(0f, rb.velocity.y, forwardSpeed);
+
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        rb.AddForce(movement * playerSpeed);
+    }
+
+    void Jump()
+    {
+        //if( Input.GetKey(KeyCode.Space) && isTouchGround == true)
+        if (Input.GetButtonDown("Jump") && isTouchGround == true)
+        {
+            Vector3 jumpForceVector = new Vector3(0f, jumpForce, 0f);
+            rb.AddForce(jumpForceVector, ForceMode.Impulse);
+            isTouchGround = false;
+            Debug.Log("jumping");
+        }
+        
+    }
+
+    private void OnCollisionStay()
+    {
+        isTouchGround = true;
     }
 }
