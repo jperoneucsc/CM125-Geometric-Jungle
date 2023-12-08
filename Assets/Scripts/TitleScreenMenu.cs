5f9e4505685fa702cae93545370be4f72e235549
jperoneucsc <jperone@ucsc.edu>
2023-12-08 15:46:18 -0800

    Revert "credits"
    This reverts commit 5f9308f1d352017a32db8f940474b105bb28c798.

--- Assets/Scripts/TitleScreenMenu.cs
+++ Assets/Scripts/TitleScreenMenu.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class TitleScreenMenu : MonoBehaviour
 {
-    public Canvas title;
-    public Canvas credits;
-
     // Start is called before the first frame update
-    public void goToCredits()
+    void Start()
     {
-        // Hide the current canvas
-        if (title != null)
-        {
-            title.gameObject.SetActive(false);
-        }
-
-        // Show the new canvas
-        if (credits != null)
-        {
-            credits.gameObject.SetActive(true);
-        }
+        
     }
-    
-    public void goToMain()
-    {
-        // Hide the current canvas
-        if (credits != null)
-        {
-            credits.gameObject.SetActive(false);
-        }
 
-        // Show the new canvas
-        if (title != null)
-        {
-            title.gameObject.SetActive(true);
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
