6a0b1ea847feb9849350fd1de7dfa27038a52f21
Sammy SlugAustin <achen168@ucsc.edu>
2023-12-08 16:29:40 -0800

    adding x axis

--- Assets/Scripts/Environment/TileSpawner.cs
+++ Assets/Scripts/Environment/TileSpawner.cs
@@ -11,16 +11,17 @@ public class TileSpawner : MonoBehaviour
     public GameObject groundTile;
     Vector3 nextSpawnPoint;
 
-
-    public void SpawnTile() 
+    
+    public void SpawnTile()
     {
+        float horizontalOffset = Random.Range(-2.0f, 2.0f);
         secNum = Random.Range(0, 3);
         GameObject temp = Instantiate(section[secNum], nextSpawnPoint, Quaternion.identity);
-        
+
         bool spawnItem = Random.Range(0f, 1f) > 0.5f;
         if (spawnItem)
         {
-            itemSpawner.SpawnItem(temp.transform.position + Vector3.up * 1.0f);
+            itemSpawner.SpawnItem(temp.transform.position + Vector3.up * 1.0f + Vector3.right * horizontalOffset);
         }
         nextSpawnPoint = temp.transform.GetChild(0).transform.position;
     }
@@ -28,9 +29,10 @@ public class TileSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
         {
             SpawnTile();
         }
     }
 }
+
